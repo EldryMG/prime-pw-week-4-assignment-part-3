22b5211f4ac1649030563cb6bcf6d13bedc67292
Franck Bushbaum <franck.bushbaum@yahoo.com>
2021-08-07 21:49:07 -0500

    Completed Assignment Week 4 Part 3 stretch goal #1.

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,44 +2,55 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 let basket = [];
+const maxItems = 5
 
-function addItem(item){
-   basket.push(item);
-   return true;
+
+function addItem(item) {
+  basket.push(item);
+  isFull();
+  return true;
 }
 
-console.log(`Basket is ${basket.length}`);
-console.log('Adding soda (expect true)', addItem('soda'));
-console.log(`Basket is now ${basket}`);
-console.log(`Adding sushi bowl and soup to basket`, addItem(`sushi bowl`), addItem(`soup`));
-console.log(`Basket now has ${basket.length} items,${basket}.`)
+console.log('Adding almonds (expect true)', addItem('almonds'));
+console.log('Adding cheese (expect true)', addItem('cheese'));
+
 console.log(`Adding coffee`, addItem(`coffee`));
-console.log(`Basket now has ${basket.length} items,${basket}.`)
-
-
-function listItems(anArray){
-  for( i=0; i<anArray.length; i++){
-    console.log(i, anArray[i]);
-  } return `Basket has ${anArray.length} items`
-}
-
+console.log('Adding steak (expect true)', addItem('steak'));
+console.log('Adding soda (expect true)', addItem('soda'));
+console.log(`Adding sushi bowl to basket`, addItem(`sushi bowl`));
 console.log(`The basket has ${listItems(basket)} in it.`);
-console.log(basket);
+empty();
+console.log(addItem('cake'));
+console.log(addItem('juice'));
+console.log(addItem('granola bar'));
+console.log(addItem('pie'));
+console.log(addItem('pizza')); // Adds item.
+console.log(addItem('hot pockets')); // function isFull() returns true and keeps item out.
 
-//console.log(basket.length); // currently equals 4.
-var currentBasket;
+function listItems(anArray) {
+  for (i = 0; i < anArray.length; i++) {
+    console.log(i, anArray[i]);
+  }
+  return `Basket has ${anArray.length} items`
+}
 
-function empty(){
-   return basket = [];
+
+
+
+function empty() {
+  return basket = [];
 
 }
-console.log(empty());
-console.log( `The basket has ${basket.length} items.`);
-
-console.log(`Basket has ${basket.length} items in it.`);
-console.log('Adding water (expect true)', addItem('water'));
-console.log(`Basket is now ${basket}`);
-console.log(`Adding spinach and bananas to basket`, addItem(`spinach`), addItem(`bananas`));
-console.log(`Basket now has ${basket.length} items,${basket}.`)
-console.log(`Adding Apple Cider Vinegar`, addItem(`A.C.V`));
-console.log(`Basket now has ${basket.length} items, ${basket} .`)
+// Stretch
+function isFull() {
+  for (i = 0; i < basket.length; i++) {
+    if (basket.length <= maxItems) {
+      console.log(`${basket} in basket.`)
+      return false;
+    } else {
+      basket.pop(i);
+      console.log(`Too many items.`)
+      return true;
+    }
+  }
+}
